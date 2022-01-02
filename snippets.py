65b693c1cbba99234413152565120780dd91f0ea
Razhan Hameed <razhan13@gmail.com>
2022-01-02 22:37:46 +0000

    policy iteration

--- snippets.py
+++ snippets.py
@@ -127,7 +127,8 @@ class FrozenLake(Environment):
             # UTF-8 arrows look nicer, but cannot be used in LaTeX
             # https://www.w3schools.com/charsets/ref_utf_arrows.asp
             # actions = ['^', '<', '_', '>']
-            actions = ['▲', '◄', '▼', '►']
+            actions = ['↑', '←', '↓', '→']
+
 
             print('Lake:')
             print(self.lake)
@@ -171,16 +172,34 @@ def play(env):
 ################ Model-based algorithms ################
 
 def policy_evaluation(env, policy, gamma, theta, max_iterations):
-    value = np.zeros(env.n_states, dtype=np.float)
+    value = np.zeros(env.n_states, dtype=float)
 
     # TODO:
-
+    delta = np.inf 
+    while delta >= theta:
+        delta = 0
+        for s in range(env.n_states):
+            Vold = value[s]
+            v_s = 0
+            for n_s in range(env.n_states):
+                v_s += env.p(n_s, s, policy[s]) * (env.r(n_s, s, policy[s]) + gamma * value[n_s])
+            value[s] = v_s
+            delta = max(delta, abs(Vold - value[s]))
     return value
     
 def policy_improvement(env, value, gamma):
     policy = np.zeros(env.n_states, dtype=int)
     
     # TODO:
+    q_table = np.zeros((env.n_states, env.n_actions))
+    for s in range(env.n_states):
+        for a in range(env.n_actions):
+            v_s = 0
+            for n_s in range(env.n_states):
+                v_s += env.p(n_s, s, a) * (env.r(n_s, s, a) + gamma * value[n_s])
+            q_table[s,a] = v_s
+
+    policy = q_table.argmax(axis = 1)
 
     return policy
     
@@ -191,6 +210,13 @@ def policy_iteration(env, gamma, theta, max_iterations, policy=None):
         policy = np.array(policy, dtype=int)
     
     # TODO:
+    value = np.zeros(env.n_states, dtype=float)
+    prev_policy = None
+
+    while not (prev_policy == policy).all():
+        prev_policy = policy
+        value = policy_evaluation(env, policy, gamma, theta, max_iterations)
+        policy = policy_improvement(env, value, gamma)
         
     return policy, value
     
@@ -334,11 +360,11 @@ def main():
     
     print('')
     
-    # print('## Policy iteration')
-    # policy, value = policy_iteration(env, gamma, theta, max_iterations)
-    # env.render(policy, value)
+    print('## Policy iteration')
+    policy, value = policy_iteration(env, gamma, theta, max_iterations)
+    env.render(policy, value)
     
-    # print('')
+    print('')
     
     # print('## Value iteration')
     # policy, value = value_iteration(env, gamma, theta, max_iterations)
