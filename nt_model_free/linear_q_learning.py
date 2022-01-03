2254587c0354c8dbc1c0670d96c8fbfb65a6dce9
sumanthreddy9 <sumanthreddy4512@gmail.com>
2022-01-04 04:45:07 +0530

    added nt q leanring and sarsa code

--- nt_model_free/linear_q_learning.py
+++ nt_model_free/linear_q_learning.py
@@ -1,16 +1,31 @@
 import numpy as np
 
+
 def linear_q_learning(env, max_episodes, eta, gamma, epsilon, seed=None):
     random_state = np.random.RandomState(seed)
-    
+
     eta = np.linspace(eta, 0, max_episodes)
     epsilon = np.linspace(epsilon, 0, max_episodes)
-    
+
     theta = np.zeros(env.n_features)
-    
+
     for i in range(max_episodes):
         features = env.reset()
-        
-        # TODO:
 
-    return theta   
+
+        # TODO:
+        q = features.dot(theta)
+        terminal = False
+        while not terminal:
+            if np.random.rand() < 1 - epsilon[i] and max(q) != 0:
+                a = np.argmax(q)
+            else:
+                a = np.random.choice(env.n_actions)
+            nxt_feat, r, terminal = env.step(a)
+            delta_val = r - q[a]
+            q = nxt_feat.dot(theta)
+            delta_val += gamma*max(q)
+            theta += eta[i]*delta_val*features[a]
+            features = nxt_feat
+
+    return theta
--- nt_model_free/linear_sarsa.py
+++ nt_model_free/linear_sarsa.py
@@ -15,5 +15,21 @@ def linear_sarsa(env, max_episodes, eta, gamma, epsilon, seed=None):
         q = features.dot(theta)
 
         # TODO:
-    
+        is_terminal = False
+        if np.random.rand() < (1 - epsilon[i]) and max(q) != 0:
+            a = np.argmax(q)
+        else:
+            a = np.random.choice(env.n_actions)
+        while not is_terminal:
+            nxt_feat, r, is_terminal = env.step(a)
+            delta_val = r - q[a]
+            q = nxt_feat.dot(theta)
+            if np.random.rand() < (1 - epsilon[i]) and max(q) != 0:
+                nxt_act = np.argmax(q)
+            else:
+                nxt_act = np.random.choice(env.n_actions)
+            delta_val += gamma * q[nxt_act]
+            theta += eta[i] * delta_val * features[a]
+            features = nxt_feat
+            a = nxt_act
     return theta
