9682f3d44d54b8d961746e4cc0bc1ec81cd679bd
Razhan Hameed <razhan13@gmail.com>
2022-01-04 03:01:29 +0000

    implementing transitional probabilities for the grid

--- environment/frozen_lake.py
+++ environment/frozen_lake.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from .environment import Environment, _printoptions
+from itertools import product
 
 class FrozenLake(Environment):
     def __init__(self, lake, slip, max_steps, seed=None):
@@ -32,10 +33,51 @@ class FrozenLake(Environment):
         # TODO:
         Environment.__init__(self, n_states, n_actions, max_steps, None)
 
-        # Up 0, down 1, left 2, right 3
+        #               Up      left      down     right 
         self.actions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
+        self.idx_to_states = [] 
+        # Indices to states (coordinates), 
+        for i in range(self.lake.shape[0]):
+            for j in range(self.lake.shape[1]):
+                self.idx_to_states.append((i, j))
+        # print(self.idx_to_states)
+        # State to indicies ( we use this as lookup table to make sure the state is part of our grid)
+        self.states_to_idx = {s: i for (i, s) in enumerate(self.idx_to_states)}
+        # print(self.states_to_idx)
+        
+
+        # Precomputed transition probabilities
+        self.tansition_probs = np.zeros((self.n_states, self.n_states, self.n_actions))
+        
+        for state_idx, state in enumerate(self.idx_to_states):
+            for action_idx, action in enumerate(self.actions):
+                if self.lake[state] == '$' or self.lake[state] == '#':
+                    next_state_idx = self.absorbing_state
+                else:
+                    next_state = (state[0] + action[0], state[1] + action[1])
+                    # print("next_state", next_state)
+                    # If next_state is not valid, default to current state index ( meaning if next state is outside the grid)
+                    if next_state in self.states_to_idx:
+                        next_state_idx = self.states_to_idx[next_state]
+                    else:
+                        next_state_idx = state_idx
+                    # print("next_state_idx", next_state_idx)
+
+                # However, with probability 0.1, the environment ignores the desired direction 
+                # and the agent slips (moves one tile in a random direction)
+                for a_idx, a in enumerate(self.actions):
+                    if a == action:
+                        self.tansition_probs[next_state_idx, state_idx, a_idx] += 0.9 # There is 0.9 probability you move to a different tile
+                    self.tansition_probs[next_state_idx, state_idx, a_idx] += self.slip / (self.n_actions)
+                    # print("Slip chance", self.slip / (self.n_actions))
+
+        # Transition probabilities for the absorbing state
+        for a_idx, a in enumerate(self.actions):
+            self.tansition_probs[self.absorbing_state, self.absorbing_state, a_idx] += 1
+
         # Load the precomputed transition probabilities
-        self._p = np.load('./p.npy')
+        # self.tansition_probs = np.load('./p.npy')
         
     def step(self, action):
         state, reward, done = Environment.step(self, action)
@@ -46,7 +88,7 @@ class FrozenLake(Environment):
         
     def p(self, next_state, state, action):
         # TODO:
-        return self._p[next_state, state, action]
+        return self.tansition_probs[next_state, state, action]
 
     def r(self, next_state, state, action):
         # TODO:
--- test_env.py
+++ test_env.py
@@ -1,5 +1,5 @@
 from environment.frozen_lake import FrozenLake 
-
+import numpy as np
 
 seed = 0
 # Small lake
@@ -8,7 +8,22 @@ lake =   [['&', '.', '.', '.'],
           ['.', '.', '.', '#'],
           ['#', '.', '.', '$']]
 
-env = FrozenLake(lake, slip=0.1, max_steps=16, seed=seed)
+# Large lake
+largeLake = [['&', '.', '.', '.', '.', '.', '.', '.'],
+             ['.', '.', '.', '.', '#', '.', '.', '.'],
+             ['.', '.', '.', '.', '.', '.', '#', '.'], 
+             ['.', '.', '.', '.', '#', '.', '.', '.'], 
+             ['.', '.', '.', '#', '.', '.', '.', '.'], 
+             ['.', '#', '#', '.', '.', '.', '#', '.'], 
+             ['.', '#', '.', '.', '#', '.', '#', '.'],   
+             ['.', '.', '.', '#', '.', '.', '.', '$']]
+
+lake_size = np.array(largeLake)
+max_steps = lake_size.shape[0] * lake_size.shape[1] # 64
+# print(max_steps)
+
+
+env = FrozenLake(largeLake, slip=0.1, max_steps=max_steps, seed=seed)
 done = False 
 while not done:
     actions = ['w', 'a', 's', 'd']
